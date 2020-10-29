f1bb2a3637f5eb2464a20d4976a515a5ef959f40
Matthieu <matthieu.daugas@epitech.eu>
2020-10-29 15:27:28 +0100

    [ADD] routes users and clocks

--- backend/lib/backend_web/router.ex
+++ backend/lib/backend_web/router.ex
@@ -38,7 +38,6 @@ defmodule BackendWeb.Router do
     get "/vacations/:userID", VacationController, :get_all_vacations
 
   end
-
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
