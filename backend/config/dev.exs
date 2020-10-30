544aa265ff8226c66625652481f7cbe11bc29983
NUNES <thomas.nunes@epitech.eu>
2020-10-30 09:47:19 +0000

    [TEST] CI

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -5,7 +5,7 @@ config :backend, Backend.Repo,
   username: "postgres",
   password: "root",
   database: "backend_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
