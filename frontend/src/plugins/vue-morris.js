ce1e98f8751d6cfb9334e15167e8bc80023a3baf
Matthieu <matthieu.daugas@epitech.eu>
2020-10-31 11:47:54 +0100

    [FIX] bar chart

--- frontend/src/plugins/vue-morris.js
+++ frontend/src/plugins/vue-morris.js
@@ -1,6 +1,5 @@
 import Raphael from 'raphael/raphael'
 import jQuery from 'jquery'
 
-// This is needed by Morris
 global.Raphael = Raphael
 global.jQuery = jQuery
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack');
 module.exports = {
     configureWebpack: {
       plugins: [
-        //'/plugins/vue-morris'
         new webpack.ProvidePlugin({jQuery: 'jquery'})
       ],
     },
